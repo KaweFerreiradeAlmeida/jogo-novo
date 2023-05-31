520195fa89f2b6f062c58aa657f1303a93188e5a
KaweFerreiradeAlmeida <121455367+KaweFerreiradeAlmeida@users.noreply.github.com>
2023-05-31 15:33:11 -0300

    Add files via upload

---
+++ Jogo-autoral-main/sketch.js
@@ -0,0 +1,23 @@
+var piloto,piloto1
+var dinheiro,money
+var obstacle,obstacle1
+var
+var
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+
+}
+
+function draw(){
+  background("white");
+
+
+  drawSprites();
+
+}
